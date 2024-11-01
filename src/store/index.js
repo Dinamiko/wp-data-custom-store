df3d13e3a66dc4a805b99c77925395610b932274
Emili Castells Guasch <e.castells@inpsyde.com>
2024-11-01 16:36:53 +0100

    Add plugins store

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,49 @@
 import {createReduxStore, register} from '@wordpress/data';
 
-const DEFAULT_STATE = {
-    data: [],
-    loading: false,
-    error: '',
+const actions = {
+    setPlugins(plugins) {
+        return {
+            type: 'set_plugins',
+            plugins
+        }
+    },
+    fetch(term) {
+        return {
+            type: 'FETCH',
+            term,
+        };
+    },
 }
 
-const store = createReduxStore('plugins', {
-    reducer(state = DEFAULT_STATE, action) {
+export const store = createReduxStore('plugins', {
+    reducer(state = {plugins: []}, action) {
         switch (action.type) {
-            default:
-                return state;
+            case 'set_plugins':
+                return {...state, plugins: action.plugins}
+        }
+
+        return state;
+    },
+    actions,
+    selectors: {
+        getPlugins(state) {
+            return state.plugins
+        }
+    },
+    controls: {
+        FETCH(action) {
+            return fetch(`https://api.wordpress.org/plugins/info/1.2/?action=query_plugins&search=${action.term}`)
+                .then(response => response.json())
+                .then(json => {
+                    return json.plugins
+                })
+        },
+    },
+    resolvers: {
+        * getPlugins(term) {
+            const plugins = yield actions.fetch(term)
+
+            return actions.setPlugins(plugins);
         }
     }
 });
